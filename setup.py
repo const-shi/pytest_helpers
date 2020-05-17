0aec7eb3d4c58ef6dbd9866afd536645e323da72
const-shi <>
2020-05-17 19:20:58 +0300

    add license and url for pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup
 setup(
     name="pytest_helpers",
     version="0.1.0",
-    description='pytest helpers',
+    description="pytest helpers",
+    url="https://github.com/const-shi/pytest_helpers",
+    license="MIT License",
     author="const-shi",
     packages=["pytest_helpers"],
     # the following makes a plugin available to pytest
